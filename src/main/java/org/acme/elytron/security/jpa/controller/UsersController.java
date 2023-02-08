70ba01b9108360fe2d735daaa7bd8ec609232c51
helvizar <helvizaranderson@gmail.com>
2023-02-08 10:52:04 +0700

    Basic auth with security-jpa

---
+++ src/main/java/org/acme/elytron/security/jpa/controller/UsersController.java
@@ -0,0 +1,20 @@
+package org.acme.elytron.security.jpa.controller;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+
+@Path("/api/users")
+public class UsersController {
+
+    @GET
+    @RolesAllowed("user")
+    @Path("/me")
+    public String me(@Context SecurityContext securityContext) {
+        return securityContext.getUserPrincipal().getName();
+    }
+}
---
+++ src/main/java/org/acme/elytron/security/jpa/service/Startup.java
@@ -0,0 +1,20 @@
+package org.acme.elytron.security.jpa.service;
+
+import io.quarkus.runtime.StartupEvent;
+import org.acme.elytron.security.jpa.model.User;
+
+import javax.enterprise.event.Observes;
+import javax.inject.Singleton;
+import javax.transaction.Transactional;
+
+@Singleton
+public class Startup {
+
+    @Transactional
+    public void loadUsers(@Observes StartupEvent evt) {
+        //reset and load all users
+        User.deleteAll();
+        User.add("admin", "admin", "admin");
+        User.add("user", "user", "user");
+    }
+}
